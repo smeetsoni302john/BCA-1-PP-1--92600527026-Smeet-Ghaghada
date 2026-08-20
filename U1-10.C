0491b30b64d27edf207453f3969ef7f0b41f64b2
smeetsoni302john <smeetsoni.302john@gmail.com>
2026-08-20 15:38:37 +0530

    Add files via upload

---
+++ U1-10.C
@@ -0,0 +1,22 @@
+//WAP to display sum of the number
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	int x,y,z;
+	clrscr();
+
+	printf("\n Enter value of x : ");
+	scanf("%d", &x);
+
+	printf("\n Enter value of y : ");
+	scanf("%d", &y);
+
+	z=x+y;
+	printf("\n %d Addition is the answer",z);
+
+	getch();
+
+}
