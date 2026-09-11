16987530985fb1ef1ee7d84edf6c8ff0bf3b5bce
smeetsoni302john <smeetsoni.302john@gmail.com>
2026-09-11 10:05:33 +0530

    Add files via upload

---
+++ DEFINING.C
@@ -0,0 +1,18 @@
+//Define Sybolic Constants
+
+#include<stdio.h>
+#include<conio.h>
+
+#define PI 3.14
+
+void main()
+
+{
+	float r=2.5,a;
+	clrscr();
+
+	a=PI*r*r;
+	printf("\n Area is : %f ",a);
+
+	getch();
+}
---
+++ ESCAPESE.C
@@ -0,0 +1,18 @@
+//global variable demo
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+	int i;
+	clrscr();
+
+	for(i=1;i<=2000;i++)
+
+	{
+		printf("\a Thank You ");
+	}
+	getch();
+}
